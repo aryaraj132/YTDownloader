280fb26841f97a7797ac6518ea71de4e7f9013c1
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2020-11-06 00:36:37 +0530

    improved GUI
    Updated to take input and applied self method

--- YT_GUI.py
+++ YT_GUI.py
@@ -2,42 +2,42 @@ try:
     from PyQt5 import QtWidgets
     from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
     import sys
-    from pytube import YouTube
-    from pytube import Playlist
+    #from pytube import YouTube
+    #from pytube import Playlist
 except Exception as e:
     print("some modules are missing {}".format(e))
 
-def setvalue(str,str1):
-    url = str
-    path = str1
-    print(url, path)
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200,70,1000,900)
+        self.setWindowTitle("Gui with Aryan")
+        self.initUI()
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Enter URL")
+        self.label.move(50,50)
+        self.textbox = QLineEdit(self)
+        self.textbox.move(200,40)
+        self.textbox.resize(280,40)
+        self.label1 = QtWidgets.QLabel(self)
+        self.label1.setText("Enter Download Path")
+        self.label1.move(50,150)
+        self.label1.resize(150,15)
+        self.textbox1 = QLineEdit(self)
+        self.textbox1.move(200,140)
+        self.textbox1.resize(280,40)
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Submit")
+        self.b1.move(200,250)
+        self.label2 = QtWidgets.QLabel(self)
+        self.label2.setText("Enter resolution (“720p”, “480p”, “360p”, “240p”, “144p”)")
+        self.label2.move(50,350)
+        self.label2.resize(500,15)
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200,70,1000,900)
-    win.setWindowTitle("Gui with Aryan")
-    label = QtWidgets.QLabel(win)
-    label.setText("Enter URL")
-    label.move(50,50)
-    textbox = QLineEdit(win)
-    textbox.move(200,40)
-    textbox.resize(280,40)
-    label1 = QtWidgets.QLabel(win)
-    label1.setText("Enter Download Path")
-    label1.move(50,150)
-    label1.resize(150,15)
-    textbox1 = QLineEdit(win)
-    textbox1.move(200,140)
-    textbox1.resize(280,40)
-    b1 = QtWidgets.QPushButton(win)
-    b1.setText("Submit")
-    b1.move(200,250)
-    b1.clicked.connect(setvalue(textbox.text(), textbox1.text()))
-    label2 = QtWidgets.QLabel(win)
-    label2.setText("Enter resolution (“720p”, “480p”, “360p”, “240p”, “144p”)")
-    label2.move(50,350)
-    label2.resize(500,15)
+    win = MyWindow()
     win.show()
     sys.exit(app.exec_())
 window()
