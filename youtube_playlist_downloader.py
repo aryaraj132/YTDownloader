77e1aa51e197af1c9370b9a95e278e78615e7808
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2020-11-06 18:17:52 +0530

    Update youtube_playlist_downloader.py
    Exception Handeling to prevent crashing if one file of playlist is not downloaded

--- youtube_playlist_downloader.py
+++ youtube_playlist_downloader.py
@@ -1,21 +1,27 @@
 try:
     from pytube import YouTube
     from pytube import Playlist
+    import os
 except Exception as e:
     print("some modules are missing {}".format(e))
+Dir = os.path.dirname(os.path.realpath(__file__))
 count = 1
+error = 1
 url = input("Enter URL to download : ")
-path = input("Enter Path to download to : ")
+path = Dir
 playlist = Playlist(url)
 res = input("Enter resolution (“720p”, “480p”, “360p”, “240p”, “144p”) : ")
 for video_url in playlist.video_urls:
-    try:
-        ytd = YouTube(video_url)
-        stream = ytd.streams.filter(progressive=True)
-        file = stream.get_by_resolution(res)
-        file.download(path)
-        print("{} File Downloaded successfully".format(count))
-        count+=1
-    except:
-        print("error found please provide proper resolution ")
-        break
+    while(True):
+        try:
+            ytd = YouTube(video_url)
+            stream = ytd.streams.filter(progressive=True)
+            file = stream.get_by_resolution(res)
+            file.download(path)
+            print("{} File Downloaded successfully".format(count))
+            count+=1
+            break
+        except:
+            print("{} File Not downloaded".format(error))
+            error+=1
+            break
